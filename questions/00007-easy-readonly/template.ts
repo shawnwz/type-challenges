d0e29b13a32d1c3e3221fba29e2c9f38772a4638
wzhe@outlook.com <wzhe@outlook.com>
2022-05-09 16:28:54 +0800

    pick, readonly, tuple to object

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,17 @@
-type MyReadonly<T> = any
+type MyReadonly<T> = {
+    readonly [P in keyof T]: T[P];
+}
+
+
+// js
+
+// function readonly (obj) {
+
+//     const result = {};
+
+//     for (const key in obj) {
+//         result["readonly" + key] = obj[key];
+//     }
+
+//     return result;
+// }
--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,19 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly any[]> = {
+    [P in T[number]]: P;
+};
+
+const arr = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type t = TupleToObject<typeof arr>
+
+
+
+//js 
+
+// function tupleToObject (array) {
+//     const obj = {};
+//     array.forEach((val)=>{
+//         obj[val] = val;
+//     })
+//     return obj;
+// }
