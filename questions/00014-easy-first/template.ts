6bee9bf70eba2903712a45427a22c20aeceb502e
wzhe@outlook.com <wzhe@outlook.com>
2022-05-10 16:56:38 +0800

    array first

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,13 @@
-type First<T extends any[]> = any
+type First<T extends any[]> = T extends [] ? never : T[0]
+
+type First2<T extends any[]> = T["length"] extends 0 ? never : T[0]
+
+type First3<T extends any[]> = T[0] extends T[number] ? T[0] : never
+
+type First4<T extends any[]> = T extends [infer first, ...infer rest] ? first : never
+
+//js
+// const first = (arr) => {
+//     // if arr is empty return never
+//     return arr[0]
+// }
