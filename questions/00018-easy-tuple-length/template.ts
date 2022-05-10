ccd5b7d80d999967847339faa8b48a1e16277826
wzhe@outlook.com <wzhe@outlook.com>
2022-05-10 17:35:56 +0800

    18 tuple length

--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,1 +1,10 @@
-type Length<T> = any
+type Length<T extends readonly any[]> = T["length"]
+
+
+
+//js
+
+// const length = (arr) => {
+//     if(Array.isArray(arr)) return;
+//     return arr.length;
+// }
