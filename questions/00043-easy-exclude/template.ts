a8ccf185d1507540af81a754aa7fb28371e9412e
wzhe@outlook.com <wzhe@outlook.com>
2022-05-11 10:39:54 +0800

    189 awaited

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,11 @@
 type MyExclude<T, U> = T extends U ? never : T
+
+
+type a = 'a' | 'b' | 'c' | 'd'
+type b = 'c'
+type c = 'e' | 'f'
+type d = 'g'
+type z = MyExclude<c, d>
+
+let test : a
+test = 'b'
--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,4 @@
-type MyAwaited = any
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X> 
+                                                ? X extends Promise<unknown> 
+                                                    ? MyAwaited<X> : X 
+                                                : T
