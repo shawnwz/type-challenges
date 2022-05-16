142e9300e44e079435df93f6b19bdacbac84233b
wzhe@outlook.com <wzhe@outlook.com>
2022-05-16 17:53:50 +0800

    includes

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,10 @@
-type Includes<T extends readonly any[], U> = any
+//type Includes<T extends readonly any[], U> = U extends T[number] ? true : false;
+
+import { Equal } from "@type-challenges/utils"
+
+type Includes<T extends readonly any[], U> = T extends [infer F, ... infer R]
+    ? Equal<U, F> extends true
+    ? true
+    : Includes<R, U> : false;
+
+export default Includes;
